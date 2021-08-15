0d4c12b9107b706816683ca945a94e0afc1df0da
roshni.mishra <roshni.mishra@teknopoint.in>
2021-08-15 21:36:20 +0530

    Latest changes by churkin

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const expressLayouts = require('express-ejs-layouts');
 
-
 const appRouter = require('./routes');
 
 const app = express();
@@ -26,18 +25,18 @@ app.use('/', appRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error', { title: 'Roshni Mishra' });
 });
 
 module.exports = app;
